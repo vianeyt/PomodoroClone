ed79edee7795bcc1852d208060e9a9a6ef729845
Vianey Tlapechco <vianey.tlapechco@gmail.com>
2024-01-15 22:30:43 -0500

    Working on adding a new countdown timer with progress indicator

--- main.js
+++ main.js
@@ -8,6 +8,48 @@ const timer = {
 
 };
 
+
+// circle variables
+const semicircles = document.querySelectorAll('.semicircle');
+
+//input
+const hr = 0;
+const min = 0;
+const sec = 0;
+
+const hours = hr * 3600000;
+const minutes = min * 60000;
+const seconds = sec * 1000;
+const setTime = hours + minutes + seconds;
+const starTime = Date.now();
+const futureTime = starTime + setTime;
+
+
+const timerLoop = setInterval(countDownTimer);
+countDownTimer();
+
+function countDownTimer(){
+    const currentTime = Date.now();
+    const remainingTime = futureTime - currentTime;
+    const angle = (remainingTime / setTime) * 360;
+    //progress indicator
+    if(angle > 180){
+        semicircles[2].style.display = 'none';
+        semicircles[0].style.transform = 'rotate(180deg)';
+        semicircles[1].style.transform = `rotate(${angle}deg)`;
+    } else {
+        semicircles[2].style.display = 'block';
+        semicircles[0].style.transform = 'rotate(180deg)';
+        semicircles[1].style.transform = `rotate(${angle}deg)`;
+    }
+    // timer
+
+    // 5sec-condition
+
+    //end
+}
+//
+
 let interval;
 
 const buttonSound = new Audio('button-sound.mp3');
